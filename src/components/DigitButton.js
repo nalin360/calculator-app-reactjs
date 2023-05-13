90d531c21c356e4b4b4abbddc78de4a8f04421dc
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-05-13 17:38:43 +0530

    button components with onclick => prams

---
+++ src/components/DigitButton.js
@@ -0,0 +1,11 @@
+import { ACTIONS } from "../App.js"
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
