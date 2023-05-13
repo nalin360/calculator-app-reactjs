038c8a2c422836c001bc88e88ab2ec4e143b1c49
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-05-13 22:35:42 +0530

    .

--- src/components/DigitButton.js
+++ src/components/DigitButton.js
@@ -3,8 +3,7 @@ import { ACTIONS } from "../App.js"
 export default function DigitButton({ dispatch, digit }) {
   return (
     <button
-      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
-    >
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>
       {digit}
     </button>
   )
